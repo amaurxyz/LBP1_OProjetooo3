a9a6cd1a99c8cb7d59879ac5caaf55797d6473b8
amaurxyz <129287512+amaurxyz@users.noreply.github.com>
2024-10-17 15:00:16 -0300

    controller.py

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for,session
 from models.model import validacao
 
 loginController = Blueprint('loginController', __name__)
@@ -9,12 +9,17 @@ def login():
         nome = request.form['nome']
         senha = request.form['senha']
         if validacao(nome, senha):
+            session['nome']= nome
+            session['senha'] =senha
             return redirect(url_for('loginController.dashboard'))
         else:
-            error = 'Credências Erradas'
+            error = 'CredÃªncias Erradas'
             return render_template("index.html", error=error)
     return render_template("index.html")
 
 @loginController.route("/dashboard")
 def dashboard():
-    return render_template("dashboard.html")
+    if 'nome' in session:
+        return render_template("dashboard.html")
+    else:
+       return redirect(url_for('loginController.login'))
