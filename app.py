80827b757024ebf7c640ea3efbd86258138fd60a
amaurxyz <129287512+amaurxyz@users.noreply.github.com>
2024-10-18 08:10:26 -0300

    app.py

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-from flask import Flask, request,session
+from flask import Flask, request, session, redirect, url_for
 from controllers.controller import loginController
 from datetime import timedelta
 
 app = Flask(__name__)
-
-app.secret_key = 'chave'
-app.permanent_session_lifetime = timedelta(minutes=5)
+app.secret_key = 'ifsp'
+app.permanent_session_lifetime = timedelta(days=1)
 
 @app.before_request
 def log_request_info():
-    print(f'MÃ©todo: {request.method}, URL: {request.url}')
+    print(f'Método: {request.method}, URL: {request.url}')
+
+@app.before_request
+def require_login():
+    if 'logged' not in session and request.endpoint == 'loginController.dashboard':
+        return redirect(url_for('loginController.login'))
 
 app.register_blueprint(loginController)
 
