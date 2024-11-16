4a967ad1ad069e303e4cc6589208f59673513849
amaurxyz <129287512+amaurxyz@users.noreply.github.com>
2024-11-16 15:06:03 -0300

    model.py

--- projeto_paginacao/models/model.py
+++ projeto_paginacao/models/model.py
@@ -1,4 +1,4 @@
-# models/model.py
+# Cria uma lista de 30 itens que armazena produtos com id, nome e decrição
 meusprodutos = [
     {"id": i, "nome": f"Produto {i}", "descricao": f"Descrição do produto {i}"} for i in range(1, 31)
 ]
